f99fdfc1581297d6d735f01829fe67ccee13442c
Jelle Besseling <jelle@pingiun.com>
2021-07-21 17:59:26 +0200

    Add serial console

--- src/nixos/configuration.nix
+++ src/nixos/configuration.nix
@@ -9,6 +9,13 @@ with utils;
   boot.loader.grub.device = "nodev";
   boot.cleanTmpDir = true;
 
+  boot.kernelParams = [ "console=ttyS0,19200n8" ];
+  boot.loader.grub.extraConfig = ''
+    serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
+    terminal_input serial;
+    terminal_output serial
+  '';
+
   fileSystems."/" = { device = "/dev/sda"; fsType = "ext4"; };
   swapDevices = [{ device = "/dev/sdb"; }];
   boot.initrd.postMountCommands = ''
